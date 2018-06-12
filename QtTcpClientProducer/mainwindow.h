b10d29b1637534b514cfae468791497902fc9175
lucasgsr14 <lucasgsr14@hotmail.com>
2018-06-12 20:35:41 -0300

    adicionando comentários

--- QtTcpClientProducer/mainwindow.h
+++ QtTcpClientProducer/mainwindow.h
@@ -20,11 +20,30 @@ public:
 
 public slots:
   void putData();
+  /**
+   * @brief tcpConnect método usado para se conectar com o server
+   */
   void tcpConnect();
+  /**
+   * @brief tcpDisconnect método usado para se desconectar com o server
+   */
   void tcpDisconnect();
+  /**
+   * @brief deslocaSlider método usado para movimentar o slider do mínimo caso o slider máximo tente ser menor que o mínimo
+   */
   void deslocaSlider();
+  /**
+   * @brief impedeMaiorMenor método usado para deslocar o slider máximo para ser sempre maior ou igual o mínimo
+   */
   void impedeMaiorMenor();
+  /**
+   * @brief stopData método usado para parar de enviar os dados, ou seja, mata o temporizador.
+   */
   void stopData();
+  /**
+   * @brief timerEvent para enviar os dados de acordo com a quantidade de milissegundos fornecidos pelo usuário
+   * @param e
+   */
   void timerEvent(QTimerEvent *e);
 private:
   Ui::MainWindow *ui;
